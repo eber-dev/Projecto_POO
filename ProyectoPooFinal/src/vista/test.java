3a77481013c746508ccbbff168e165190f6d936e
Eber <www.eberacunaquispe@gmail.com>
2026-01-20 19:42:14 -0500

    Algunas pruebas de las variantes del metodo actualizar hasta ahora todo correcto

--- ProyectoPooFinal/src/vista/test.java
+++ ProyectoPooFinal/src/vista/test.java
@@ -17,34 +17,19 @@ import modelo.productoDAO;
 public class test {
     public static void main(String[] args) {
         
-        /*producto p = new producto("Mouse", 25.5, 10);
         productoDAO dao = new productoDAO();
-
-        if (dao.insertar(p)) {
-            System.out.println("Insertado correctamente");
+        producto producto = new producto("Teclado 2.0",90,8,4);
+        
+        /*if(dao.insertar(producto)){
+            System.out.println("Producto insertado exitosamente");
         }*/
         
-        
-        
-        productoDAO dao = new productoDAO();
-        List<producto> productos =dao.listar();
-        
-        for(producto p: productos){
-            System.out.println(p.getNombre());
-            System.out.println(p.getId());
+        if(dao.actualizar_precio(56, 120)){
+            System.out.println("El precio fue cambiado exitosamente");
+        }else{
+            System.out.println("BAKA BAKA BAKA muuuu");
         }
         
-        /*String url = "jdbc:sqlite:data/almacen.db";
-        
-        try(Connection conn = DriverManager.getConnection(url)){
-            if(conn !=null){
-                System.out.println("Conexion Exitosa");
-            }
-        }catch(SQLException e){
-            System.out.println("Error de conexion");
-            e.printStackTrace();
-        }*/
         
     }
-    
 }
