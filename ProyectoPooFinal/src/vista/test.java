0b717d41d4d2910cab7b5138f3528cb096218c31
Eber <www.eberacunaquispe@gmail.com>
2026-01-22 18:00:38 -0500

    Prueba de insertar un movimiento con un id inexistente correcta SQLite lo detecta y no lo ingresa en la base de datos

--- ProyectoPooFinal/src/vista/test.java
+++ ProyectoPooFinal/src/vista/test.java
@@ -5,12 +5,16 @@
 package vista;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import modelo.UsuarioDAO;
 import modelo.producto;
 import modelo.productoDAO;
 import java.util.Scanner;
+import modelo.Movimiento;
+import modelo.MovimientosDAO;
 import modelo.Usuario;
 
 /**
@@ -18,16 +22,29 @@ import modelo.Usuario;
  * @author EBER
  */
 public class test {
-    public static void main(String[] args) {
-        String a,b;
+    public static void main(String[] args) throws SQLException {
+        /*String a,b;
         Scanner lector = new Scanner(System.in);
         System.out.println("Ingrese su usuario: ");
         a= lector.nextLine();
         System.out.println("Ingrese su contraseña: ");
         b= lector.nextLine();
         UsuarioDAO dao = new UsuarioDAO();
-        dao.login(a,b);
+        dao.login(a,b);*/
         
+        MovimientosDAO mov = new MovimientosDAO();
+        /*List<Movimiento> transaccion = mov.listar();
+        
+        for(Movimiento e: transaccion){
+            System.out.println(e.getTipo());
+        }*/
+        
+        Movimiento ga = new Movimiento(6,"ENTRADA",18,"2026-01-22");
+        if(mov.insertar(ga)){
+            System.out.println("mal");
+        }else{
+            System.out.println("nice");
+        }     
     }
         
 }
