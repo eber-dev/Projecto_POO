9e233ac054ed23d11608e3582c7f1f41cda8b902
Eber <www.eberacunaquispe@gmail.com>
2026-01-22 11:40:13 -0500

    Se agrego el metodo listar que ejecuta un select en la base de datos osea muetra los datos

---
+++ ProyectoPooFinal/src/modelo/MovimientosDAO.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+/**
+ *
+ * @author EBER
+ */
+public class MovimientosDAO {
+    public List<Movimiento> listar(){
+        List<Movimiento> lista = new ArrayList<>();
+        String url = "jdbc:sqlite:data/almacen.db";
+        try(Connection conexion = DriverManager.getConnection(url)){
+            Statement stmt = conexion.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT id_movimiento,id_producto,tipo,cantidad,fecha FROM movimientos_stock");
+            while(rs.next()){
+                int id_movimiento = rs.getInt("id_movimiento");
+                int id_producto = rs.getInt("id_producto");
+                String tipo = rs.getString("tipo");
+                int cantidad = rs.getInt("cantidad");
+                String fecha = rs.getString("fecha");
+                
+                Movimiento mov = new Movimiento(id_movimiento,id_producto,tipo,cantidad,fecha);
+                lista.add(mov);
+            }
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        
+        return lista;
+    }
+    
+}
