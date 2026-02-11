6a473f8dce9691d00fff76edc80b949d16c73cdf
Eber <www.eberacunaquispe@gmail.com>
2026-02-11 18:52:44 -0500

    Se añadio el metodo limitarspinner que limita la cantidad a 7 para ingresar las categorias

--- ProyectoPooFinal/src/vista/FrmGestion.java
+++ ProyectoPooFinal/src/vista/FrmGestion.java
@@ -58,6 +58,7 @@ public class FrmGestion extends javax.swing.JFrame {
         IngresosSalidas();
         lista();
         usuarios();
+        limitarspiner();
     }
     
     public void IngresosSalidas(){
@@ -96,6 +97,14 @@ public class FrmGestion extends javax.swing.JFrame {
             modelo4.addRow(new Object[]{p.getId_usuario(),p.getNombre(),p.getApellido(),p.getUsuario(),p.getPassword(),p.getRol()});
         }
     }
+    
+    public void limitarspiner(){
+        SpinnerNumberModel spin1 = new SpinnerNumberModel(1,1,7,1);
+        SpinnerNumberModel spin2 = new SpinnerNumberModel(1,1,7,1);
+        ActualizarCategoria.setModel(spin1);
+        CategoriaNuevo.setModel(spin2);
+    }
+    
     
 
 
@@ -146,18 +155,18 @@ public class FrmGestion extends javax.swing.JFrame {
         jLabel29 = new javax.swing.JLabel();
         jLabel30 = new javax.swing.JLabel();
         jLabel32 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
+        ActualizarProducto = new javax.swing.JButton();
         jLabel11 = new javax.swing.JLabel();
         jLabel34 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
-        jButton3 = new javax.swing.JButton();
+        EliminarProducto = new javax.swing.JButton();
         ActualizarNombre = new javax.swing.JTextField();
         ActualizarPrecio = new javax.swing.JTextField();
         jLabel31 = new javax.swing.JLabel();
-        jSpinner1 = new javax.swing.JSpinner();
+        ActualizarCategoria = new javax.swing.JSpinner();
         jLabel33 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
+        ActualizarStock = new javax.swing.JTextField();
+        RecibirID = new javax.swing.JTextField();
         GestionUsuarios = new javax.swing.JPanel();
         jLabel4 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
@@ -367,11 +376,11 @@ public class FrmGestion extends javax.swing.JFrame {
         jLabel32.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
         jLabel32.setText("Nombre:");
 
-        jButton2.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
-        jButton2.setText("Actualizar Producto");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        ActualizarProducto.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
+        ActualizarProducto.setText("Actualizar Producto");
+        ActualizarProducto.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                ActualizarProductoActionPerformed(evt);
             }
         });
 
@@ -388,22 +397,22 @@ public class FrmGestion extends javax.swing.JFrame {
             }
         });
 
-        jButton3.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
-        jButton3.setText("Eliminar");
+        EliminarProducto.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
+        EliminarProducto.setText("Eliminar");
 
         ActualizarPrecio.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
 
         jLabel31.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
         jLabel31.setText("Stock:");
 
-        jSpinner1.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        ActualizarCategoria.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
 
         jLabel33.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
         jLabel33.setText("ID:");
 
-        jTextField2.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
+        ActualizarStock.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
 
-        jTextField3.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
+        RecibirID.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
 
         javax.swing.GroupLayout StockProductosLayout = new javax.swing.GroupLayout(StockProductos);
         StockProductos.setLayout(StockProductosLayout);
@@ -439,22 +448,22 @@ public class FrmGestion extends javax.swing.JFrame {
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                         .addComponent(jLabel31)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(ActualizarStock, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
                                     .addGroup(StockProductosLayout.createSequentialGroup()
                                         .addComponent(jLabel30)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(jSpinner1)
+                                        .addComponent(ActualizarCategoria)
                                         .addGap(18, 18, 18)
                                         .addComponent(jLabel33)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(RecibirID, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
                                     .addGroup(StockProductosLayout.createSequentialGroup()
                                         .addComponent(jLabel32)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                         .addComponent(ActualizarNombre))))
                             .addGroup(StockProductosLayout.createSequentialGroup()
                                 .addGap(88, 88, 88)
-                                .addComponent(jButton2)))
+                                .addComponent(ActualizarProducto)))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(StockProductosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, StockProductosLayout.createSequentialGroup()
@@ -483,7 +492,7 @@ public class FrmGestion extends javax.swing.JFrame {
                                         .addGap(112, 112, 112)
                                         .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addGap(18, 18, 18)
-                                        .addComponent(jButton3))
+                                        .addComponent(EliminarProducto))
                                     .addComponent(jLabel10))
                                 .addGap(9, 9, 9)))))
                 .addContainerGap(38, Short.MAX_VALUE))
@@ -507,7 +516,7 @@ public class FrmGestion extends javax.swing.JFrame {
                             .addComponent(jLabel34)
                             .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel11)
-                            .addComponent(jButton3))))
+                            .addComponent(EliminarProducto))))
                 .addGroup(StockProductosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(StockProductosLayout.createSequentialGroup()
                         .addGap(8, 8, 8)
@@ -530,19 +539,19 @@ public class FrmGestion extends javax.swing.JFrame {
                     .addComponent(jLabel29)
                     .addComponent(ActualizarPrecio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel31)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(ActualizarStock, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(StockProductosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel18)
                     .addComponent(CategoriaNuevo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel30)
-                    .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(ActualizarCategoria, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel33)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(RecibirID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(StockProductosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(AgregarProducto)
-                    .addComponent(jButton2))
+                    .addComponent(ActualizarProducto))
                 .addContainerGap(27, Short.MAX_VALUE))
         );
 
@@ -782,9 +791,9 @@ public class FrmGestion extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField1ActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void ActualizarProductoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ActualizarProductoActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_ActualizarProductoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -812,8 +821,11 @@ public class FrmGestion extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JSpinner ActualizarCategoria;
     private javax.swing.JTextField ActualizarNombre;
     private javax.swing.JTextField ActualizarPrecio;
+    private javax.swing.JButton ActualizarProducto;
+    private javax.swing.JTextField ActualizarStock;
     private javax.swing.JButton AgregarProducto;
     private javax.swing.JTextField CampoContraseña;
     private javax.swing.JTextField CampoNombre;
@@ -823,18 +835,18 @@ public class FrmGestion extends javax.swing.JFrame {
     private javax.swing.JSpinner CategoriaNuevo;
     private javax.swing.JButton CerrarSesion;
     private javax.swing.JComboBox<String> Eleccion;
+    private javax.swing.JButton EliminarProducto;
     private javax.swing.JButton Exportar;
     private javax.swing.JPanel GestionUsuarios;
     private javax.swing.JPanel MovimientoVentas;
     private javax.swing.JTabbedPane Multipanel;
+    private javax.swing.JTextField RecibirID;
     private javax.swing.JPanel StockProductos;
     private javax.swing.JTable TablaIngresos;
     private javax.swing.JTable TablaLista;
     private javax.swing.JTable TablaSalidas;
     private javax.swing.JTable TablaUsuarios;
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
@@ -876,9 +888,6 @@ public class FrmGestion extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JScrollPane jScrollPane4;
-    private javax.swing.JSpinner jSpinner1;
     private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
     // End of variables declaration//GEN-END:variables
 }
